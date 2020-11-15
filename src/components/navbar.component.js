406e533ca3dd7cc4489b1fdad16b48765d005a2f
BerghoutFatima <berghoutfatima2@gmail.com>
2020-11-15 14:15:20 +0100

    mycommit

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -19,6 +19,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
                   </div>
                 </div>
               </nav>
+              
             </div>
             </Router>
             
